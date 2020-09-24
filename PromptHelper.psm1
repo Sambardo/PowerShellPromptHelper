f6451d89b8d980e31c0a492274e7010ba0053138
Kory T <korythacher@gmail.com>
2020-09-24 16:10:58 -0700

    my first commit

---
+++ PromptHelper.psm1
@@ -0,0 +1,109 @@
+<#MISC TODO
+1. make time work better/differently
+2. add osme preset prompts
+3. help data
+4. github
+5. gallery
+#>
+
+#region variables
+#used to maintain whether they toggled title bar paths on or off so it can set back to a default UI path if they turn it off
+$script:TiteleBar = $false
+#endregion
+
+#region private functions
+Function Prompt_Append_Helper
+{
+    [CmdletBinding()]
+    param($promptText = "PowerShellRocks",
+          $dbg = "[DBG]",
+          $divider = "> ",
+          [switch]$emoji)
+
+    if($emoji)
+    {
+        $promptText = "$promptText🤘"
+        $dbg = "🐜"
+    }
+
+    if ($PSDebugContext)
+    {
+        $promptText = "$dbg $prompt"
+    }
+
+    if ($host.name -eq "ServerRemoteHost")
+    {
+        $promptText = "[$env:ComputerName] $prompt"
+    }
+
+    $promptText + $divider
+}
+
+
+
+#endregion
+
+<# CURRENTLY REPLACED WITH CALL TO SET-PROMPT at the bottom. This is due to extra prompt being required anyways to trigger this
+function prompt 
+{
+    #$Time = (Get-Date).ToString("hh:mm") 
+    Set-Prompt -promptText "💪🐚" -emoji -TitlebarPath
+}
+#>
+
+#region Public Functions
+function Set-Prompt
+{
+    [cmdletbinding()]
+    param(
+        $promptText = "PowerShellRocks",
+        $dbg = "[DBG]",
+        $divider = "> ",
+        [switch]$emoji,
+        [switch]$TitlebarPath,
+        [switch]$Time #TODO make this time thing work better. Do I put it before/after/inplace of prompt text?
+    )
+   
+    #Build the code for the new prompt, then set the code in the global prompt function
+    $code = ""
+    if($TitlebarPath)
+    {
+        $code+='$host.ui.rawui.WindowTitle = (Get-Location)'
+        $script:TiteleBar = $true
+    }
+    elseif($script:TiteleBar)#if they previously had the title bar path and no longer do then it needs to change to a default
+    {
+        $host.ui.rawui.WindowTitle = "PowerShell"
+    }
+
+   if($Time)
+    {
+            $promptText = "`$((get-date).ToString('hh:mm')) $promptText"
+    }
+
+    $code+="`nPrompt_Append_Helper -promptText `"$promptText`"  -dbg '$dbg'  -divider '$divider'"
+    if($emoji){$code+=" -emoji"}
+
+    set-content Function:\global:prompt -value $code
+}
+
+#TODO refactor and integrate as parameter set for Set-Prompt
+function Select-Prompt
+{
+    [cmdletbinding()]
+    param([ValidateSet("Simple Time with Title Path","Emoji with Title Path","Rocks with Title Path")] $selection)
+
+    switch($selection)
+    {
+        "Simple Time with Title Path"   {Set-Prompt -promptText "" -TitlebarPath -time}
+        "Emoji with Title Path"         {Set-Prompt -promptText "💪🐚" -emoji -TitlebarPath}
+        "Rocks with Title Path"         {Set-Prompt -TitlebarPath}
+    }
+}
+
+
+#endregion
+
+#Triggered on load to use default prompt 
+#TODO pick a more neutral one
+Set-Prompt -promptText "💪🐚" -emoji -TitlebarPath
